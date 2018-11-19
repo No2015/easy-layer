a2879c7054220c52ca734e3b53f6eb92272dbcca
陈其文 <jianwang19@sina.com>
2018-11-19 21:02:32 +0800

    初始化

---
+++ src/lib/msg.vue
@@ -0,0 +1,47 @@
+<template>
+	<div :class="classname" v-html="content"></div>
+</template>
+
+<script>
+export default {
+    name: 'Msg',
+    props: {
+        options: {
+            type: Object,
+            default(){
+                return {}
+            }
+        }
+    },
+    created(){
+        setTimeout(() => {
+            this.$layer.close(this.layero)
+            this.yes(this.layero)
+        }, this.time)
+    },
+    mounted(){
+        this.init()
+    },
+    data () {
+        return {
+            content: this.options.content,
+            time: this.options.time,
+            layero: this.options.layero,
+            yes: this.options.yes,
+            zIndex: this.options.zIndex,
+            maxWidth: this.options.maxWidth,
+            classname: 'layer-dialog layer-msg layer-dialog-anima layer-dialog-' + this.options.skin
+        }
+    },
+    methods: {
+    	init(){
+            let $el = this.$el
+            let o = $el.getBoundingClientRect()
+            $el.style.marginLeft = -o.width/2 + 'px'
+            $el.style.marginTop = -o.height/2 + 'px'
+            $el.style.zIndex = this.zIndex
+            $el.style.maxWidth = this.maxWidth + 'px'
+        }
+    }
+}
+</script>
